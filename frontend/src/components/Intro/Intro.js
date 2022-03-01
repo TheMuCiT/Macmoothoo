d2503f7200b831f93eca43d326ab8c30c3bc2b78
THeMuCiT127 <themucit127@gmail.com>
2022-03-02 01:08:04 +0200

    FOOTER

--- frontend/src/components/Intro/Intro.js
+++ frontend/src/components/Intro/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { ReactComponent as Rectangle } from '../../icons/Rectangle.svg'
 
@@ -8,6 +8,23 @@ import Img2 from '../../images/IMG2.png'
 import './intro.css'
 
 const Intro = () => {
+  const countdownDate = new Date('May 24, 2022 00:00:00 GMT+10:00').getTime()
+  const [
+    {
+      expired,
+      values: { days, hours, minutes, seconds },
+    },
+    setResult,
+  ] = useState(() => calculateCountdownFromNow(countdownDate))
+
+  useEffect(() => {
+    if (expired) return undefined
+    const intervalId = setInterval(() => setResult(calculateCountdownFromNow(countdownDate)), 1000)
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [expired, countdownDate])
+
   const handleGetStarted = () => {
     //TODO
   }
@@ -35,6 +52,29 @@ const Intro = () => {
             </div>
           </div>
         </div>
+        <div className='timerCounter introTimer'>
+          <div className='timerContainer'>
+            <div className='timerBox'>
+              <div className='timerBoxValue'>{('0' + days).slice(-2)}</div>
+              <div className='timerBoxText'>Days</div>
+            </div>
+
+            <div className='timerBox'>
+              <div className='timerBoxValue'>{('0' + hours).slice(-2)}</div>
+              <div className='timerBoxText'>Hours</div>
+            </div>
+
+            <div className='timerBox'>
+              <div className='timerBoxValue'>{('0' + minutes).slice(-2)}</div>
+              <div className='timerBoxText'>Minutes</div>
+            </div>
+
+            <div className='timerBox'>
+              <div className='timerBoxValue'>{('0' + seconds).slice(-2)}</div>
+              <div className='timerBoxText'>Seconds</div>
+            </div>
+          </div>
+        </div>
         <div className='introRight SlideRightAnimation'>
           <div className='introRightWrapper'>
             <div className='introRightBackground'>
@@ -52,3 +92,31 @@ const Intro = () => {
 }
 
 export default Intro
+
+export const calculateCountdownFromNow = (nextDate) => {
+  const now = new Date().getTime()
+
+  const distance = new Date(nextDate).getTime() - now
+
+  if (distance < 0) {
+    return {
+      expired: true,
+      values: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    }
+  }
+
+  return {
+    expired: false,
+    values: {
+      days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((distance % (1000 * 60)) / 1000),
+    },
+  }
+}
